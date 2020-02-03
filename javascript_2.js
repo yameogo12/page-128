196959641a97fa9201585a7fef264d96cc9fd879
samuel <samuelsyameogo@gmail.com>
2020-02-03 09:24:47 -0800

    changesw

---
+++ javascript_2.js
@@ -0,0 +1,11 @@
+function subtraction_Function() {
+    var Subtraction = 5-2;
+    document.getElementById("Math").innerHTML= Subtraction;
+}
+function addition_Function(){
+    var addition = 2 + 2;
+    addition = addition.toString().fontcolor("red")
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+
+
